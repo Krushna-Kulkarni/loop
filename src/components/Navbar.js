135853303e9c92c08fd3f17e872c9ff4d708ecb9
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2024-02-16 23:50:02 +0530

    style: brand name font

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,20 +3,19 @@ import Searchbar from "./Searchbar";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center h-18 p-2 border-b border-gray-600 bg-black">
+    <div className="flex items-center h-20 p-2 pl-8 border-b border-gray-600 bg-black">
       <div className="flex items-center text-xl gap-2">
         <img
-          className="w-8 h-4"
+          className="w-12 h-"
           src={require("../assets/images/logo.png")}
           alt="brand-logo"
         />
-        <span className="flex items-center text-2xl font-bold text-white">
-          Loop
+        <span className="flex items-center text-3xl font-bold font-mono text-white">
+          loop
         </span>
       </div>
-      <div className="flex w-[600px] justify-center ml-80">
-        <Searchbar />
-      </div>
+
+      <Searchbar />
     </div>
   );
 };
