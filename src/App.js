0870bc278c5e0197d8d96e978a1c0245a675c75a
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2024-02-17 23:56:53 +0530

    refactor + feat : moved sidebar to the top and added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Feed from "./components/Feed";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Feed />
+      <div className="flex ">
+        <div className="w-60">
+          <Sidebar />
+        </div>
+        <Routes>
+          <Route path="/" component={<Feed />} />
+        </Routes>
+      </div>
     </div>
   );
 }
